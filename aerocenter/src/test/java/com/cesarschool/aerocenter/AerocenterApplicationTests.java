a9c0abeb365adf2e75906e8e94fc64a38be3d317
ticogafa <tgafa@cesar.school>
2025-08-14 09:53:23 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ aerocenter/src/test/java/com/cesarschool/aerocenter/AerocenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarschool.aerocenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerocenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
