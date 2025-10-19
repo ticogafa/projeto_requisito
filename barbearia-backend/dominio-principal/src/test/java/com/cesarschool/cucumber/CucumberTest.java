7ad4b020a2266dce8ec9fdfb9f1d5bbd8e3e30aa
vinicius diniz amorim simoes <viniciusdinizsimoes@gmail.com>
2025-10-19 19:14:12 -0300

    Atualiza configuração do Cucumber para usar recursos gerais e glue padrão

--- barbearia-backend/dominio-principal/src/test/java/com/cesarschool/cucumber/CucumberTest.java
+++ barbearia-backend/dominio-principal/src/test/java/com/cesarschool/cucumber/CucumberTest.java
@@ -7,8 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features/ControleCaixa.feature",
-    glue = "com.cesarschool.cucumber.gestaoCaixa",
+    features = "src/test/resources/features",
+    glue = "com.cesarschool.cucumber",
     plugin = {"pretty", "html:target/cucumber-report.html"},
     monochrome = true,
     snippets = CucumberOptions.SnippetType.CAMELCASE
